1db5f4812fa58affec92de9ed164190a93ebf2c5
Anton Nagorny <nagorny.anton@gmail.com>
2022-08-27 00:25:38 +0300

    Initial Commit

---
+++ Instagram Design/Instagram_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_DesignApp.swift
+//  Instagram Design
+//
+//  Created by Anton Nagornyi on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
